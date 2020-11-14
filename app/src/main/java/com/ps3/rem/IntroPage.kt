2a3b418437ba2c336da64ba7f9755e073978d9d3
Pranav Ajay <pranav.ajay2000@gmail.com>
2020-11-14 14:13:26 +0530

    Changes Made Additions: - Background Updated :) - CV ided TODO:  - Updateable logic for Dashboard!  - Fb update acc to schema  - UI for all activites

--- app/src/main/java/com/ps3/rem/IntroPage.kt
+++ app/src/main/java/com/ps3/rem/IntroPage.kt
@@ -3,6 +3,7 @@ package com.ps3.rem
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -14,9 +15,23 @@ class IntroPage : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ac_intropage)
 
+
+        setContentView(R.layout.ac_intropage)
+        val status: Boolean? = getSharedPreferences(getString(R.string.loginstatus),Context.MODE_PRIVATE).getBoolean(getString(R.string.openstatus),false)
         gs = findViewById(R.id.btngs)
+
+        gs!!.setOnClickListener {
+            if (!status!!) {
+
+                val sp = getSharedPreferences(getString(R.string.loginstatus), Context.MODE_PRIVATE)
+                sp.edit().putBoolean(getString(R.string.openstatus), true).apply()
+                Log.d("Summa sp",sp.toString())
+            }
+            val int = Intent(applicationContext, SignUI::class.java)
+            startActivity(int)
+
+        }
 
     }
 
@@ -30,9 +45,6 @@ class IntroPage : AppCompatActivity() {
 
                 val sp = getSharedPreferences(getString(R.string.loginstatus), Context.MODE_PRIVATE)
                 sp.edit().putBoolean(getString(R.string.openstatus), true).apply()
-                val int = Intent(applicationContext, SignUI::class.java)
-                startActivity(int)
-
             }
             val int = Intent(applicationContext, SignUI::class.java)
             startActivity(int)
